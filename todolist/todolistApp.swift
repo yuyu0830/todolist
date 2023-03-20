dc36c6d22e4d77af9d80293f650dd4ccdb4abb4c
yuyu0830 <yuyu0830@naver.com>
2023-03-20 19:21:14 +0900

    Create repository

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by mac on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
